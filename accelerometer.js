5b0a13d652f4ba3fd43072045968e7b231ed42af
Tom Selvi <tomselvi@gmail.com>
2015-11-13 13:09:46 -0800

    Basic start

---
+++ accelerometer.js
@@ -0,0 +1,44 @@
+var I2C = require('i2c');
+var LSM9DS0 = require('../registers/lsm9ds0');
+
+var binary = function(i) {
+  return Number.parseInt(i, 2);
+};
+
+var Accelerometer = function(address) {
+  if (!address) address = LSM9DS0.ACC_ADDRESS;
+  this.address = address;
+  this.i2c = new I2C(address, {device: '/dev/i2c-0'});
+};
+
+module.exports = Accelerometer;
+
+Accelerometer.prototype.write = function(command, byte, callback) {
+  this.i2c.writeBytes(command, [byte], callback);
+};
+
+Accelerometer.prototype.read = function(command, callback) {
+  this.i2c.readBytes(command, 1, callback);
+};
+
+Accelerometer.prototype.initialize = function(callback) {
+  this.write(LSM9DS0.CTRL_REG1_XM, binary('01100111'), function(err) {
+    if (err) throw err;
+    this.write(LSM9DS0.CTRL_REG2_XM, binary('00100000'), callback);
+  });
+};
+
+Accelerometer.prototype.readACCX = function(callback) {
+  var acc_l = this.read(LSM9DS0.OUT_X_L_A, function(err, res) {
+    if (err) throw err;
+    callback(res);
+  });
+};
+
+var berryIMU = new Accelerometer();
+berryIMU.initialize(function(err) {
+  if (err) throw err;
+  berryIMU.readACCX(function(res) {
+    console.log(res);
+  });
+});
---
+++ index.js
@@ -0,0 +1,7 @@
+var BerryIMU = {
+  accelerometer: require("./accelerometer"),
+  gyroscope: require("./gyroscope"),
+  magnetometer: require("./magnetometer")
+};
+
+module.exports = BerryIMU;
